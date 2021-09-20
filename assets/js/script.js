d25a55d2d32adbc8d5bdf2bb3a84dfe0569cbeae
MrTofuuu <chris.stallcup@gmail.com>
2021-09-20 10:44:19 -0500

    formatted js prior to working on weather rating

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,11 @@ function sendApiRequest() {  // promise
     return data.json() // returns data 
     })
     .then(function(json){
-   console.log(json.data[0].images.fixed_height.url) // this will alow us to filter down to what we're looking for from the GIPHY api: images, normal height and url to display GIF to browser
-  var imgPath = json.data[0].images.fixed_height.url // associated to the how the GIF is dislayed in the browser
-  var img = document.createElement("img") 
-                              
-  img.setAttribute("src", imgPath) 
- document.body.appendChild(img) // appends image to the browser 
-  })
-    
-  }
+    console.log(json.data[0].images.fixed_height.url) // this will alow us to filter down to what we're looking for from the GIPHY api: images, normal height and url to display GIF to browser
+    var imgPath = json.data[0].images.fixed_height.url // associated to the how the GIF is dislayed in the browser
+    var img = document.createElement("img") 
+
+    img.setAttribute("src", imgPath) 
+    document.body.appendChild(img) // appends image to the browser 
+    })
+}
