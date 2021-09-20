f0a0fd8e61967588cb95488e11f07efff93e7b9f
mac owner <macowner@macs-MacBook-Pro.local>
2021-09-19 22:09:12 -0500

    Applying materialize css, htmland also including weather logic in this commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,84 +6,87 @@
 // API link for 5-day forecast: https://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={API key}+&units=imperial
 // API link for icons: https://openweathermap.org/img/wn/{ICON ID}@2x.png
 
+// BOXES SHOULD CONTAIN: ICON + TEMP + WIND + HUMIDITY + UVINDEX
+// UPPER BOX: CURRENT + NEXT FOUR DAYS
 
-// Variables ... these need to be defined in html
+
+// Declaring variables
 var button = document.querySelector('.searchButton')
 var inputValue = document.querySelector('.searchInput')
 
-// NEED TO CONNECT THESE VARIABLES TO HTML
 var cityValue = document.querySelector('.city')
 var tempValue = document.querySelector('.temp')
+var humidValue = document.querySelector('.humidity')
 var descValue = document.querySelector('.desc')
+var windValue = document.querySelector('.wind')
 var uvIndexValue = document.querySelector('.uvIndex')
+// vars to hide and display weather and Gif boxes
+var weatherDisplayBox = document.querySelector('.weatherDisplayBox')
 
-
-// PULLING DATA FOR CURRENT WEATHER
 button.addEventListener('click', function() {
-    // for current temperature
+    // fetching current conditions
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=a3be7588e2f22d761077e844f13fff0c'+"&units=imperial")
     .then(response => response.json())
     .then(response => {
-       
-        // NEED TO CHECK IF THESE ARE VALID ACCORDING TO DESIGN
-        cityValue.innerHTML = response.name;
-        tempValue.innerHTML = "Temperature: "+response.main.temp+"°F";
+        // editing inner html 
+        cityValue.innerHTML = response.name+" today";
+        tempValue.innerHTML = "Temp: "+response.main.temp+"°F";
+        humidValue.innerHTML = "HUM: "+response.main.humidity+" %";
+        windValue.innerHTML = "Wind: "+response.wind.speed+" MPH";
+        getUvi(response.coord.lat, response.coord.lon)
 
-        //icon
+        //icon property
         var weathericon= response.weather[0].icon;
         var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
         $(descValue).html("<img src="+iconurl+">");
 
-        // calling uvi after establishing connection to lat and long
-        getUvi(response.coord.lat, response.coord.long)
+        // showing weather box and current weather box after click
+        weatherDisplayBox.style.display="block";
     })
-    // fetching lat and long for uvi data
-    function getUvi(lat, long){
-        var getUviUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+long +"&exclude=minutely,hourly,alerts&appid=a3be7588e2f22d761077e844f13fff0c"+"&units=imperial"
-        fetch(getUviUrl)
-        .then(response => response.json())
-        .then(response => {
-        // console.log(response)
-        uvIndexValue.innerHTML = "UVI Index: "+response.current.uvi;
-        // console.log(response.daily)
-        // sending data to:
-        Fivedayforecast(response.daily)
-        })
-    }
 })
 
-
-// PULLING DATA FOR 5-DAY FORECAST
-function Fivedayforecast(fiveDay){
-    // console.log(fiveDay)
-     var getFiveUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=a3be7588e2f22d761077e844f13fff0c'+"&units=imperial"
-    $.ajax({
-        url:getFiveUrl,
-        method:"GET"
-    }).then(function(response){
-
-    for (let i=1; i < 6; i++) {
-        // console.log("this is day",fiveDay[i])
-        var day = moment.unix(fiveDay[i].dt).format("MM/DD/YYYY")
-        var temp = response.list[i+1].main.temp;
-        // uv index will show as it was established inside the getUvi function
-        // /icon property.
-        var weathericon= response.list[i+1].weather[0].icon;
-        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-        
-   
-
-        $("#day-"+i).html(day);
-        $("#desc-"+i).html("<img src="+iconurl+">");
-        $("#temp-"+i).html(temp+"°F");
-        $("#wind-"+i).html(wind+" MPH");
-        $("#humidity-"+i).html(humidity+" %");
-    
-    }
+// function to establish latitude and longitude to pull uvi data
+function getUvi(lat, lon){
+    var getUviUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon +"&exclude=minutely,hourly,alerts&appid=a3be7588e2f22d761077e844f13fff0c"+"&units=imperial"
+    // fetching one call
+    fetch(getUviUrl)
+    .then(response => response.json())
+    .then(response => {
+    // innerhtml for index value
+    uvIndexValue.innerHTML = "UVI: "+response.current.uvi;
+    Nextdaysforecast(response.daily)
     })
 }
 
 
-// PULLING DATA FOR HOURLY
-
+function Nextdaysforecast(NextDays){
+    // console.log(NextDays)
+     var getNextDaysUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=a3be7588e2f22d761077e844f13fff0c'+"&units=imperial"
+    $.ajax({
+        url:getNextDaysUrl,
+        method:"GET"
+    })
+    .then(function(response){
+    // loop to run through next days
+    for (let i=1; i < 6; i++) {
+        console.log("this is day",NextDays[i])
+        var day = moment.unix(NextDays[i].dt).format("MM/DD/YYYY")
+        var temp = "Temp: "+response.list[i+1].main.temp;
+        var clouds = "Clouds: "+response.list[i+1].clouds.all;
+        var wind ="Winds: "+ response.list[i+1].wind.speed;
+        var humidity ="HUM: "+response.list[i+1].main.humidity;
+        // /icon property
+        var weathericon= response.list[i+1].weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        
+        // editing inner html 
+        $("#day-"+i).html(day);
+        $("#desc-"+i).html("<img src="+iconurl+">");
+        $("#temp-"+i).html(temp+"°F");
+        $("#clouds-"+i).html(clouds+" %");
+        $("#wind-"+i).html(wind+" MPH");
+        $("#humidity-"+i).html(humidity+" %");
+    }
+    })
+}
 
